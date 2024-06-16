be930c41f25b59bcc7f68b7dfd20a11a2bc5a052
KannedKielbasa1 <ldv23c.lgonzalez@uartesdigitales.edu.mx>
2024-06-16 17:02:35 -0600

    Player Inputs
    Se implementó el sistema de playerinputs pero tiene un error de compilación. (SOLUCIONAR)

--- Assets/Scripts/BasicMovement.cs
+++ Assets/Scripts/BasicMovement.cs
@@ -1,14 +1,41 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class BasicMovement : MonoBehaviour
 {
     public Animator animator;
     public float speed = 5.0f;
+    public float jumpForce = 5.0f;
 
     private float screenHalfWidthInWorldUnits;
     private float screenHalfHeightInWorldUnits;
+
+    private PlayerControls controls;
+    private Vector2 moveInput;
+    private bool jumpInput;
+
+    void Awake()
+    {
+        controls = new PlayerControls();
+
+        controls.Player.Move.performed += ctx => moveInput = ctx.ReadValue<Vector2>();
+        controls.Player.Move.canceled += ctx => moveInput = Vector2.zero;
+
+        controls.Player.Jump.performed += ctx => jumpInput = true;
+        controls.Player.Jump.canceled += ctx => jumpInput = false;
+    }
+
+    void OnEnable()
+    {
+        controls.Enable();
+    }
+
+    void OnDisable()
+    {
+        controls.Disable();
+    }
 
     void Start()
     {
@@ -21,11 +48,16 @@ public class BasicMovement : MonoBehaviour
 
     void Update()
     {
-        float moveInput = Input.GetAxisRaw("Horizontal");
-        animator.SetFloat("Horizontal", moveInput);
-
-        Vector3 horizontal = new Vector3(moveInput * speed * Time.deltaTime, 0.0f, 0.0f);
+        // MOVIMIENTO HORIZONTAL
+        Vector3 horizontal = new Vector3(moveInput.x * speed * Time.deltaTime, 0.0f, 0.0f);
         transform.position += horizontal;
+
+        // MOVIMIENTO VERTICAL (SALTO)
+        if (jumpInput)
+        {
+            transform.position += new Vector3(0.0f, jumpForce * Time.deltaTime, 0.0f);
+            jumpInput = false; // SALTA UNA VEZ POR APACHURRAR
+        }
 
         // LOOP PARA PASAR AL PERSONAJE AL OTRO LADO DE LA PANTALLA
         Vector3 newPosition = transform.position;
@@ -37,6 +69,15 @@ public class BasicMovement : MonoBehaviour
         else if (newPosition.x > screenHalfWidthInWorldUnits)
         {
             newPosition.x = -screenHalfWidthInWorldUnits;
+        }
+
+        if (newPosition.y < -screenHalfHeightInWorldUnits)
+        {
+            newPosition.y = screenHalfHeightInWorldUnits;
+        }
+        else if (newPosition.y > screenHalfHeightInWorldUnits)
+        {
+            newPosition.y = -screenHalfHeightInWorldUnits;
         }
 
         transform.position = newPosition;
